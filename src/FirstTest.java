6f17168b64b5add2ef7b20b15e02b48fef099bfc
olgerrda <olgerrda@gmail.com>
2025-06-17 23:06:37 +0400

    Ex1: Configuration of instruments

---
+++ src/FirstTest.java
@@ -0,0 +1,40 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.text.DecimalFormat;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel9");
+        capabilities.setCapability("platformVersion", "15");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", "/Users/olga/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apks/org-wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("First test run");
+    }
+}
